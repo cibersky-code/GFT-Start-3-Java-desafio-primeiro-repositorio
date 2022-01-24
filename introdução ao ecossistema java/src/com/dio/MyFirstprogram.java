375bf0baba28cdfb6f0559f4c26166b635aca4aa
cibersky-code <cibersky@gmail.com>
2022-01-24 13:48:37 -0300

    alteração no código

--- introdução ao ecossistema java/src/com/dio/MyFirstprogram.java
+++ introdução ao ecossistema java/src/com/dio/MyFirstprogram.java
@@ -1,11 +1,15 @@
-package com.Dio 
+package com.dio;
+
+import com.dio.base.Order;
 
 
 public class MyFirstprogram{
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	
-}
+		final Order order1 = new Order("123215646");
+
+		System.out.println(order1);
+	}	
 
 }
