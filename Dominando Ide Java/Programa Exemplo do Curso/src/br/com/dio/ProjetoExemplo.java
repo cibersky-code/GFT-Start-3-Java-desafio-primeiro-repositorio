1413ab347e23f4f7108f2761ce9c73df08ca1acb
cibersky-code <cibersky@gmail.com>
2022-01-25 15:37:41 -0300

    Primeiro commit

---
+++ Dominando Ide Java/Programa Exemplo do Curso/src/br/com/dio/ProjetoExemplo.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class ProjetoExemplo {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+		System.out.println(gato);
+		/*
+		 * int a = 1, b = 4; System.out.println("Hello Word= " + (a + b));
+		 */
+	}
+}
