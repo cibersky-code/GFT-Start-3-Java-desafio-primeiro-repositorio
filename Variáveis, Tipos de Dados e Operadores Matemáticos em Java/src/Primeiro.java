4b27a3ca9e60d2c0946f24272b5787f5db844124
cibersky-code <cibersky@gmail.com>
2022-01-26 08:39:48 -0300

    Exercicio Aula 1

---
+++ Variáveis, Tipos de Dados e Operadores Matemáticos em Java/src/Primeiro.java
@@ -0,0 +1,36 @@
+//exemplo de exercicio
+
+public class Primeiro {
+    public static void main(String[] args) {
+    int i;
+    int I;
+    //int 1;
+    int _avb;
+    int $ab;
+
+    i = 5;
+    I = 10;
+    _avb = 20;
+    $ab = 7;
+
+    final int j = 10;
+    //j = 15
+
+    int quantidadeProduto = 50;
+    int QuantidadeProduto = 200;
+    final int NUMERO_TENTATIVAS = 5;
+    int QUANTIDADE_OPCOES = 5;
+    //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_avb);
+        System.out.println($ab);
+        System.out.println(j);
+        System.out.println(quantidadeProduto);
+        System.out.println(QuantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+        System.out.println(QUANTIDADE_OPCOES);
+    }
+}
